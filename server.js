9e81946745fd81a1c0f2802155e04e16dc53b3f2
saegoogon <saegoogon@naver.com>
2026-03-02 15:50:34 +0900

    Refactor user registration endpoint and server setup
    Fix syntax errors and improve user registration logic.

--- server.js
+++ server.js
@@ -2,18 +2,22 @@ const express = require("express");
 const fs = require("fs");
 
 const app = express();
+
 app.use(express.json());
+app.use(express.static("public));  // 추가
 
-// 회원가입
-app.post("/register", (req, res) => {
-  const { username, password } = req.body;
+//회원가입
+app. post("/register",(req,res) =>{
+   const {username ,password } =req . body;
 
-  const newUser = { username, password };
+  const newUSER = { username, password };
 
-  fs.appendFileSync("users.txt", JSON.stringify(newUser) + "\n");
+  fs.appendFileSync("users.txt",JSON.stringify(newUser) + "\n");
+  res.json9({ message : "회원가입 완료! });
 
-  res.json({ message: "회원가입 완료!" });
-});
+    const PORT = process.env.PORT || 3000; //수정
 
-app.listen(3000, () => {
-  console.log("서버 실행중");
+  app listen(PORT,() =>{
+    console.log("서버실행중");
+  });
+  
