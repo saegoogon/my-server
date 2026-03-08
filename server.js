cd1150c5bde27597e37384210b0f228b6d574945
saegoogon <saegoogon@naver.com>
2026-03-08 20:39:37 +0900

    Refactor server.js for cleaner code and messages

--- server.js
+++ server.js
@@ -1,28 +1,15 @@
 const express = require("express");
-const fs = require("fs");
 
-const app = express();
+const app = express ();
 
-app.use(express.json());
-app.use(express.static("public"));  
+app.use(express.static("public"));
 
-app.get("/", (req,res) => {
-   res.sendFile(__dirname + "/public/index.html");
+app.get("/",(req, res) => {
+   res.send("서버작동중");
 });
 
+const PORT = process.env.PORT || 3000;
 
-app. post("/register",(req,res) =>{
-   const {username ,password } =req . body;
-
-  const newUser = { username, password };
-
-  fs.appendFileSync("users.txt",JSON.stringify(newUser) + "\n");
-  res.json({ message : "회원가입 완료! });
-            });
-     
-    const PORT = process.env.PORT || 3000; 
-
-  app. listen(PORT,() =>{
-    console.log("서버작동중");
-  });
-  
+app.listen(PORT, () => {
+    console.log("서버실행중");
+});
