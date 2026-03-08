0b74ce06f4b340d14e683f9c6eb14cb34bb3534a
saegoogon <saegoogon@naver.com>
2026-03-08 20:01:29 +0900

    Add route to serve index.html on root path

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const app = express();
 app.use(express.json());
 app.use(express.static("public"));  
 
+app.get("/", (req,res) => {
+   res.sendFile(__dirname + "/public/index.html");
+});
+
 
 app. post("/register",(req,res) =>{
    const {username ,password } =req . body;
